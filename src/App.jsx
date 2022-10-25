b384b8cf3c058775026a00d2aaa2ac4a5476fc5e
Lucas <lucas.edgerly@gmail.com>
2022-10-25 12:37:42 -0400

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { Header } from './components/Header'
-import './global.css'
+import { Header } from './components/header/Header';
+import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/sidebar/Sidebar';
 
 export function App() {
 
   return (
-    <Header />
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          post
+        </main>
+      </div>
+    </div>
+
   )
 }
 
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import style from './Header.module.css'
-
-export function Header() {
-  return (
-    <header className={style.header}>
-      <strong>Ignite Feed</strong>
-    </header>
-  )
-}
