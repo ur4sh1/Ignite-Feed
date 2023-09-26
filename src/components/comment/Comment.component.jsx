dfdbf5a0edb0d985f0ac2adf7acf63b005009871
Lucas <lucas.edgerly@gmail.com>
2023-09-26 17:51:07 -0400

    :sparkles: feat: add like count

--- src/components/comment/Comment.component.jsx
+++ src/components/comment/Comment.component.jsx
@@ -1,10 +1,19 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "../avatar/Avatar.component";
 import styles from "../modules/Comment.module.css";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeletComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
   }
 
   return (
@@ -29,9 +38,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
